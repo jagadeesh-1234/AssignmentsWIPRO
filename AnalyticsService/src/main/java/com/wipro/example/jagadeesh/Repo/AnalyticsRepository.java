55272de35b658e25e993a25f2902bc86e77c5a42
jagadeesh-1234 <88225690+jagadeesh-1234@users.noreply.github.com>
2025-08-10 22:59:14 +0530

    Delete AnalyticsService directory

--- AnalyticsService/src/main/java/com/wipro/example/jagadeesh/Repo/AnalyticsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.example.jagadeesh.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wipro.example.jagadeesh.Entities.QuizsCount;
-
-public interface AnalyticsRepository extends JpaRepository<QuizsCount, Long> {
-
-}
