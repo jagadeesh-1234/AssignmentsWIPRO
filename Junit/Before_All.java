2312c8bda8ef9cb225e5067d286b0c24c6480da1
jagadeesh-1234 <jagadeeshdowluri2222@gmail.com>
2025-08-10 19:44:01 +0530

    Add Junit

---
+++ Junit/Before_All.java
@@ -0,0 +1,29 @@
+package Jagadeesh;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class Before_All {
+
+    
+
+    @Test
+    void greet() {
+        System.out.println("Ramesh");
+    }
+
+    @Test
+    void greet1() {
+        System.out.println("Suresh");
+    }
+
+    @Test
+    void greet2() {
+        System.out.println("Raju");
+    }
+    
+    @BeforeAll
+    static void greetBeforeAll() {
+        System.out.println("Hello");
+    }
+}
